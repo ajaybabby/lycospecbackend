6d855056b1494fdba69efa94895ef7cfdc8e729e
ajaybabby <134777387+ajaybabby@users.noreply.github.com>
2025-04-21 13:31:24 +0530

    db updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "spec",
+  host: process.env.DB_HOST,      // cute-birds-begin.loca.lt (from .env)
+  user: process.env.DB_USER,      // your MySQL username
+  password: process.env.DB_PASSWORD,  // your MySQL password
+  database: process.env.DB_NAME,      // your database name
+  port: process.env.DB_PORT,      // 3306
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
 module.exports = pool.promise();
 
